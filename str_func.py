6031ecd29b8efb6a49b80cba3908f342b9c77406
Angelina Khalueva <sprite.spirit.t.me@gmail.com>
2023-12-06 01:01:32 +0300

    Create the func to convert a text to title.

--- str_func.py
+++ str_func.py
@@ -1,5 +1,8 @@
-
 def get_uppercase_text(text: str) -> str:
     """ return all text in uppercase """
     return text.upper()
 
+
+def get_text_in_title(text: str) -> str:
+    """ return all text in title style"""
+    return text.title()
